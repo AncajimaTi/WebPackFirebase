0ebaa9f2e3178830bf7ea1aa37c09c5b47d144a0
AncajimaTi <punisherancajima@gmail.com>
2020-11-10 19:48:29 -0500

    BottonPrevious

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,40 @@ const btnNext = document.createElement('button');
 btnNext.innerText= 'Next Page';
 document.body.append(btnNext);
 
-let lastDocument = null;
+let lastDocument: any = null;
 btnNext.addEventListener('click',()=>{
-    console.log('click');
+    
     const query = usuariosRef
-                        .orderBy('nombre');
+                        .orderBy('nombre')
+                        .startAfter(lastDocument);
 
     query.limit(2).get().then(snap => {
+        fistDocument = snap.docs[0] || null;
+        lastDocument = snap.docs[ snap.docs.length - 1] || null;
         retornaDocumentos(snap);
     })
 })
 
 btnNext.click();
+
+
+
+const btnPrev = document.createElement('button');
+btnPrev.innerText= 'Previous PAge';
+document.body.append(btnPrev);
+
+
+let fistDocument: any = null;
+btnPrev.addEventListener('click',()=>{
+    
+    const query = usuariosRef
+                        .orderBy('nombre')
+                        .endBefore(fistDocument);
+
+    query.limit(2).get().then(snap => {
+        fistDocument = snap.docs[0] || null;
+
+        lastDocument = snap.docs[ snap.docs.length - 1] || null;
+        retornaDocumentos(snap);
+    })
+})
